4ee9639d338cf97d994a4b2b61ba39a31a14277e
Alejandro ZZ <ale0720199@gmail.com>
2022-11-13 11:36:19 -0500

    login css changed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,20 +12,22 @@ import "../quote.css";
 export default function Login() {
   return (
     <Jumbotron>
-      <Form className="rounded p-4 p-sm-3">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+      <div className="loginForm">
+        <Form className="rounded p-4 p-sm-3">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     </Jumbotron>
   );
 }
